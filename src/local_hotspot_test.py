29520f2736dd15949a6659c8eb8e3e159e1782e2
Yuexi Ma <yuexima@google.com>
2017-12-22 02:51:25 -0800

    Modify packaging of nbu tests
    * move source files to src directory
    * changed 'from mobly import mobly_g3' to 'from mobly import test_runner'
    
    Test: n/a
    Bug: 67869554
    Change-Id: Ib531f68a348914eaa65da8ab9f14146853a386f7

--- src/local_hotspot_test.py
+++ src/local_hotspot_test.py
@@ -5,7 +5,7 @@ import time
 
 
 from mobly import asserts
-from mobly import mobly_g3
+from mobly import test_runner
 from utils import android_base_test
 
 SSID = 'SSID'
@@ -68,4 +68,4 @@ class LocalHotspotTest(android_base_test.AndroidBaseTest):
 
 
 if __name__ == '__main__':
-  mobly_g3.main()
+  test_runner.main()
